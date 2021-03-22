dd60f1691fb44d7d3b2738daa82d65446e73fa89
João Victor Almeida Costa <jvac65@gmail.com>
2021-03-22 00:18:03 -0300

    Revert "Revert "Small changes""
    This reverts commit b63d0a02733297e2bf9d3f4335e38479ac714ef1.

--- backend.Domain/Commands/Patient/Base/RequestBase.cs
+++ backend.Domain/Commands/Patient/Base/RequestBase.cs
@@ -5,6 +5,15 @@ namespace backend.Domain.Commands.Patient.Base
 {
     public class RequestBase: IRequest<Response>
     {
+        public RequestBase(Guid id)
+        {
+            Id = id;
+        }
+
+        protected RequestBase()
+        {
+
+        }
         public Guid Id { get; set; }
     }
 }
